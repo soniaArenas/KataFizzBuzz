9dd3faacbf8c7ea198f38ef3940ac2576bfe8bdd
Sonia Arenas <61312612+soniaArenas@users.noreply.github.com>
2020-04-15 13:17:32 +0200

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz
+    {
+        public string Fizz(int numero)
+        {
+            if (fizz(numero))
+            {
+                return "fizz";
+            }
+            return numero.ToString();
+        }
+        private bool fizz(int numero)
+        {
+            return numero % 3 == 0;
+            
+        }
+
+        public string Buzz(int numero)
+        {
+            if (buzz(numero))
+            {
+                return "buzz";
+            }
+            return numero.ToString();
+        }
+        private bool buzz(int numero)
+        {
+            return numero % 5 == 0;
+
+        }
+
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass()]
+
+    
+    
+    public class FizzBuzzTests
+    {
+        public static FizzBuzz fizzbuzz;
+        [TestInitialize]
+        public void Setup()
+        {
+            fizzbuzz = new FizzBuzz();
+        }
+
+        [TestMethod()]
+        public void FizzTest_Devuelve_un_numero_entero()
+        {
+            var numero = 4;
+         
+            var response = fizzbuzz.Fizz(numero);
+            Assert.AreEqual(numero.ToString(), response);
+
+        }
+
+        [TestMethod()]
+        public void FizzTest_devuelve_fizz_si_es_Tres()
+        {
+            var numero = 3;
+            var response = fizzbuzz.Fizz(numero);
+            Assert.AreEqual("fizz", response);
+        }
+
+        [TestMethod()]
+        public void FizzTest_devuelve_fizz_si_el_numero_es_multiplo_de_tres()
+        {
+            var numero = 6;
+            var response = fizzbuzz.Fizz(numero);
+            Assert.AreEqual("fizz", response);
+        }
+        [TestMethod()]
+        public void FizzTest_devuelve_fizz_si_el_numero_es_multiplo_de_cinco()
+        {
+            var numero = 5;
+
+            var response = fizzbuzz.Buzz(numero);
+            Assert.AreEqual("buzz", response);
+        }
+    }
+}
